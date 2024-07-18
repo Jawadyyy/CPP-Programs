2bfd7af2866cb0ce26da9e24b4373f2626e0c350
Jawad Mansoor <www.jawadmansoor37@gmail.com>
2024-07-18 11:33:01 +0500

    commit

---
+++ Building Using SetW.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+
+{
+    char w;
+    char g;
+
+    cout<<"Enter the window style for the main building: "<<endl;
+    cin>>w;
+
+    cout<<"Enter the grass style for the main building: "<<endl;
+    cin>>g;
+
+    cout<<setw(8)<<"!"<<setw(33)<<"@"<<setw(32)<<"!"<<endl<<setw(9)<<"(*)"<<setw(34)<<"(***)"<<setw(31)<<"(*)"<<endl<<setw(10)<<"(***)"<<setw(35)<<"((*****))"<<setw(30)<<"(***)"<<endl<<setw(11)<<"|#|#|#|"<<setw(35)<<"((|#|#|#|))"<<setw(30)<<"|#|#|#|"<<endl<<setw(4)<<"/\\_"<<setw(6)<<"*****"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"
+<<setw(12)<<"###########"<<setw(1)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<
+setw(6)<<"*****"<<setw(4)<<"_/\\"<<endl<<setw(2)<<"|"<<setw(4)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<
+"|"<<setw(3)<<"|"<<setw(4)<<w<<setw(3)<<"|"<<setw(2)<<"|"<<setw(2)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(2)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)
+<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<"|"<<endl<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="
+<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"
+<<setw(2)<<"_"<<setw(2)<<"_"<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="
+<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<setw(2)<<"="<<endl<<setw(2)<<"|"<<setw(5)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w
+<<setw(3)<<"|"<<setw(2)<<"|"<<setw(2)<<"|"<<setw(6)<<"|"<<setw(2)<<"|"<<setw(2)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"
+<<setw(3)<<"|"<<setw(4)<<"|"<<endl<<setw(2)<<"|"<<setw(5)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(2)<<"|"
+<<setw(2)<<"|"<<setw(6)<<"|"<<setw(2)<<"|"<<setw(2)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(4)<<"|"<<endl
+<<setw(2)<<"|"<<setw(5)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(2)<<"|"<<setw(1)<<"_"<<setw(1)<<"|"<<setw(1)
+<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(1)<<"|"<<setw(1)<<"_"<<setw(1)<<"|"<<setw(2)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)
+<<"|"<<setw(4)<<"|"<<endl<<setw(2)<<"|"<<setw(5)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w
+<<setw(3)<<"|"<<setw(2)<<"|"<<setw(2)<<"|"<<setw(6)<<"|"<<setw(2)<<"|"<<setw(2)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"
+<<setw(3)<<"|"<<setw(4)<<"|"<<endl<<setw(2)<<"|"<<setw(5)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(2)<<"|"<<setw(1)
+<<"_"<<setw(1)<<"|"<<setw(1)<<"_"<<setw(2)<<"_"<<setw(2)<<"_"<<setw(1)<<"|"<<setw(1)<<"_"<<setw(1)<<"|"<<setw(2)<<"|"<<setw(3)<<w<<setw(3)<<"|"<<setw(3)<<"|"<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w<<setw(3)<<w
+<<setw(3)<<"|"<<setw(3)<<"|"<<setw(4)<<"|"<<endl<<setw(2)<<"/"<<setw(33)<<"/"<<setw(5)<<"_"<<setw(2)<<"_"<<setw(5)<<"\\"<<setw(33)<<"\\"<<endl<<setw(1)<<"/"<<setw(4)<<g<<
+setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(8)<<"/"<<setw(5)<<"/"<<setw(1)<<"_"<<setw(2)<<"_"<<setw(1)<<"\\"<<setw(5)<<"\\"<<setw(5)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<g<<setw(3)<<setw(3)<<setw(3)<<g<<setw(3)<<g<<setw(7)<<"\\"<<endl;
+
+
+return 0;
+}
